44b9e8822fe6b882ef04327e44467b63f52c4180
0xRKX <alexberarg95@gmail.com>
2023-10-03 13:45:32 +0200

    Style and Good practices

--- projects/04-technical-test/src/App.jsx
+++ projects/04-technical-test/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import './App.css'
 
 const CAT_ENPOINT_RANDOM_CAT = `https://catfact.ninja/fact`
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
@@ -9,27 +10,39 @@ export function App() {
     const [fact, setFact] = useState(String)
     const [imageURL, setImageURL] = useState()
 
+    // UseEffect Divisors // Good practice
+
+    // To get the fact about the cats
     useEffect(() => {
 
         fetch(CAT_ENPOINT_RANDOM_CAT)
-            .then(res => res.json())
+            .then(res => {
+                // TODO: Handle error if !res.ok
+                if (!res.ok) throw new Error('Error fetching fact')
+                return res.json()
+            })
             .then(data => {
                 const { fact } = data
                 setFact(fact)
-
-                const firstWord = fact.split(' ')[0]
-                const threeFirstWords = fact.split(' ', 3).join(' ')
-                // const threeFirstWords = fact.split(' ').slice(0, 3).join(' ')
-
-                fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-                    .then(res => res.json())
-                    .then(response => {
-                        const { url } = response
-                        setImageURL(url)
-                    })
             })
 
     }, [])
+
+    // To get the image using the three first words of the fact
+    useEffect(() => {
+        if (!fact) return
+        const firstWord = fact.split(' ')[0]
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+        // const threeFirstWords = fact.split(' ').slice(0, 3).join(' ')
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+            .then(res => res.json())
+            .then(response => {
+                const { url } = response
+                setImageURL(url)
+            })
+    }, [fact])
+
 
 
     return (
