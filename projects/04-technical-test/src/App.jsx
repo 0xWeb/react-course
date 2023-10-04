33067eef35272d6745e8d4cdabe873e341b1cf1b
0xRKX <alexberarg95@gmail.com>
2023-10-04 13:29:58 +0200

    Testing  With Playwright

--- projects/04-technical-test/src/App.jsx
+++ projects/04-technical-test/src/App.jsx
@@ -23,7 +23,7 @@ export function App() {
             {fact && <p>{fact}</p>}
             {imageUrl && <img src={imageUrl} alt={'Image extracted using the first three words'} />}
 
-            <Image />
+            {/* <Image /> */}
         </main>
     )
 };
--- projects/04-technical-test/src/components/Image.jsx
+++ projects/04-technical-test/src/components/Image.jsx
@@ -7,6 +7,7 @@ export function Image() {
 
     return (
         <>
+            <Image />
             {imageUrl && <img src={imageUrl} />}
         </>
     )
