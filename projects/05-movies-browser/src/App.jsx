88f86a2fa86676bb47da83b9603542efee1909ab
0xRKX <alexberarg95@gmail.com>
2023-10-11 11:44:05 +0200

    useRef and Input Manage

--- projects/05-movies-browser/src/App.jsx
+++ projects/05-movies-browser/src/App.jsx
@@ -1,12 +1,75 @@
 
 import './App.css'
+import { useEffect, useRef, useState } from 'react';
 import { useMovies } from "./hooks/useMovies";
 import { Movies } from './components/Movies';
+
+//https://www.omdbapi.com/?apikey=4287ad07&s
+
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+    if (search === '') {
+      setError('Please, writte the name of the movie')
+      return
+    }
+    if (search.length < 3) {
+      setTimeout(() => { setError('Please, writte at least 3 characters') }, 1000)
+      return
+    }
+
+    setError(null)
+
+  }, [search])
+
+
+  return { search, updateSearch, error }
+}
 
 
 function App() {
 
   const { movies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
+
+  // With Vanilla JS
+  const handleSubmitVanilla = (event) => {
+    event.preventDefault()
+    console.log({ search });
+
+    // To get all the input data from all the inputs
+    const allFields = Object.fromEntries(
+      new window.FormData(event.target)
+    )
+    console.log(allFields);
+
+    // Get the current data from the selected input
+    const fields = new window.FormData(event.target)
+    const query = fields.get('query')
+    console.log(query);
+  }
+
+  const inputRef = useRef() // this value persits between the renders
+  // With useRef
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const value = inputRef.current.value
+    console.log(value);
+  }
+
+  const handleChange = (event) => {
+    const newQuery = event.target.value
+    if (newQuery.startsWith(' ')) return
+    updateSearch(event.target.value)
+  }
 
   return (
     <div className='page'>
@@ -14,10 +77,11 @@ function App() {
         <h1>
           Movie Browser
         </h1>
-        <form className='form'>
-          <input type="text" placeholder='Harry Potter, Matrix...' />
+        <form className='form' onSubmit={handleSubmitVanilla}>
+          <input onChange={handleChange} value={search} name='search' type="text" placeholder='Harry Potter, Matrix...' />
           <button type='submit'>Search</button>
         </form>
+        {error && <p style={{ color: 'red' }} >{error}</p>}
       </header>
 
       <main>
