ae55e334f71b220d6e632bf7f9e6cc60ebd725e3
0xRKX <alexberarg95@gmail.com>
2023-10-16 11:19:05 +0200

    Adding useRef

--- projects/05-movies-browser/src/hooks/useMovies.js
+++ projects/05-movies-browser/src/hooks/useMovies.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import { searchMovies } from '../services/movies';
+import { useRef } from 'react';
 
 export function useMovies({ search }) {
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
 
+    const previusSearch = useRef(search)
+
     const getMovies = async () => {
+
+        if (search === previusSearch.current) return
+
         try {
             setLoading(true)
             setError(null)
+            previusSearch.current = search
             const newMovies = await searchMovies({ search })
             setMovies(newMovies)
         } catch (error) {
